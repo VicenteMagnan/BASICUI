be0d446eddda7b500511b897bc52132cfffaf10d
VicenteMagnan <56714666+VicenteMagnan@users.noreply.github.com>
2021-03-12 15:11:44 +0100

    Add time picker and snackbar

--- app/src/main/java/com/example/basicui/SecondFragment.kt
+++ app/src/main/java/com/example/basicui/SecondFragment.kt
@@ -1,12 +1,16 @@
 package com.example.basicui
 
+import android.os.Build
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.TimePicker
+import androidx.annotation.RequiresApi
 import androidx.navigation.fragment.findNavController
+import com.google.android.material.snackbar.Snackbar
 
 /**
  * A simple [Fragment] subclass as the second destination in the navigation.
@@ -21,11 +25,19 @@ class SecondFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_second, container, false)
     }
 
+    @RequiresApi(Build.VERSION_CODES.M)
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         view.findViewById<Button>(R.id.button_second).setOnClickListener {
             findNavController().navigate(R.id.action_SecondFragment_to_FirstFragment)
+        }
+
+        val timePicker = view.findViewById<TimePicker>(R.id.time_picker)
+
+        view.findViewById<Button>(R.id.button_confirm).setOnClickListener {
+            val caption = "Il est -" + timePicker.hour.toString() + "h" + timePicker.minute.toString()
+            Snackbar.make(view, caption, Snackbar.LENGTH_LONG).show()
         }
     }
 }
